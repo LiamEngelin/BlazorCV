4f8282be514a33f4cf12be8ae2fe42931f6e8d14
LiamEngelin <liam.engelin@iths.se>
2025-03-16 15:02:20 +0100

    crud for projectservice

--- BlazorCV/Services/ProjectService.cs
+++ BlazorCV/Services/ProjectService.cs
@@ -11,9 +11,26 @@ namespace BlazorCV.Services
             _httpClient = httpClient;
         }
 
+        public async Task<List<Project>> GetProjects()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Project>>("api/project") ?? new List<Project>();
+        }
+
         public async Task<bool> AddProject(Project project)
         {
             var response = await _httpClient.PostAsJsonAsync("api/projects", project);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> EditProject(Project project)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"api/project/{project.Id}", project);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> RemoveProject(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"api/project/{id}");
             return response.IsSuccessStatusCode;
         }
     }
