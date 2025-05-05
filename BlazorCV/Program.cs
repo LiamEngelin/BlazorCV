a8b2d1821bd66c98701b4cddaec1d5f214d22e10
LiamEngelin <liam.engelin@iths.se>
2025-05-05 19:02:57 +0200

    working prototype projects

--- BlazorCV/Program.cs
+++ BlazorCV/Program.cs
@@ -13,7 +13,7 @@ public class Program
         // Add services to the container.
         builder.Services.AddRazorComponents()
             .AddInteractiveServerComponents();
-        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("blazorcv-api-cmf3g6h8ffb7emdc.swedencentral-01.azurewebsites.net") });
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://cvapi-h8g2cyhdg0duatgb.swedencentral-01.azurewebsites.net/") });
         builder.Services.AddScoped<ProjectService>();
 
         var app = builder.Build();
